b66d6442285a7285cc0f008bf72e24ce4f82cdb1
Jason Strimpel <jstrimpel@gmail.com>
2015-07-23 10:56:21 -0700

    fixed collection target selector bug

--- dist/flexo.common.js
+++ dist/flexo.common.js
@@ -1,6 +1,6 @@
 // Flexo, Nah, I'm just messin' with ya; you're all right.
 // ----------------------------------
-// v0.2.5
+// v0.2.7
 //
 // Copyright (c)2015 Jason Strimpel
 // Distributed under MIT license
@@ -600,7 +600,7 @@ var CollectionView = View.extend({
 
     _addItemView: function (model, collection, options) {
         var collectionDef = this._findCollection(collection);
-        var $target = this.$('[' + this.attributeNameSpace + '-collection-target="' + collectionDef.name + '"]');
+        var $target = this.$('[' + this.attributeNameSpace + '-collection-target="' + collectionDef.name + '"]').first();
         var self = this;
         options = setOptions(options);
 
